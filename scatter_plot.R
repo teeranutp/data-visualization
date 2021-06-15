ed3553a7ca912570e2d658b29cdbb541e8721cfd
Tung Teeranut <tpraisuwan@gmail.com>
2021-06-15 04:34:47 +0000

    scatter plot

---
+++ scatter_plot.R
@@ -0,0 +1,10 @@
+#Scatter plot
+#2 Variables
+#pch plot R
+
+plot(mtcars$hp, mtcars$mpg, 
+     pch = 16, 
+     col = "red",
+     main = "Relation between HP and MPG",
+     xlab = "Hourse Power",
+     ylab = "Miles Per Gallon")
